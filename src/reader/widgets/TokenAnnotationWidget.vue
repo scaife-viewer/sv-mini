cdc208ecf5ea0bbbaa983411eaba7fe36940e18d
Jacob Wegner <jacobwegner@gmail.com>
2020-02-26 16:33:45 -0600

    backport SelectedWordsWidget from scaife-skeleton as TokenAnnotationWidget

---
+++ src/reader/widgets/TokenAnnotationWidget.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="selected-tokens">
+    <div class="selected-token" v-for="token in tokens" :key="token['uuid']">
+      <pre>{{ prettyPrint(token) }}</pre>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    scaifeConfig: {
+      displayName: 'Token Annotations',
+      location: 'main',
+      singleton: true,
+    },
+    methods: {
+      prettyPrint(token) {
+        return JSON.stringify(token, null, 2);
+      },
+    },
+    computed: {
+      tokens() {
+        return [
+          {
+            idx: 0,
+            uuid: 't1.1',
+            value: 'μῆνιν',
+            lemma: 'μῆνις',
+            tag: 'n-s---fa-',
+          },
+        ];
+      },
+    },
+  };
+</script>
+<style lang="scss" scoped>
+  .selected-tokens {
+    width: 100%;
+    margin: 0 2em;
+  }
+</style>
