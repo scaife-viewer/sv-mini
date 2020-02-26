641d3aa58aababf62f5797c6421c8cdef15786c7
Jacob Wegner <jacobwegner@gmail.com>
2020-02-26 16:45:37 -0600

    factor out ReaderLine component

---
+++ src/reader/components/ReaderLine.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="line u-flex">
+    <div class="line-ref">{{ line.ref }}</div>
+    <div class="line-text" v-html="line.textContent"></div>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['line'],
+  };
+</script>
+<style lang="scss" scoped>
+  .line {
+    display: flex;
+    align-items: baseline;
+    .line-ref {
+      font-size: 10pt;
+      color: #69c;
+      font-family: 'Noto Sans';
+      min-width: 4em;
+      margin-left: 1em;
+      text-align: right;
+    }
+    .line-text {
+      margin-left: 1em;
+    }
+  }
+</style>
